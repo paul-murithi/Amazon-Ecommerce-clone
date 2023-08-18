391002153c91cbf09e55910b2841c5b0bc23e783
paul-murithi <paulmurithi518@gmail.com>
2023-08-18 19:30:05 +0300

    First commit

---
+++ Readme.md
@@ -0,0 +1,23 @@
+![Project Preview](</Screenshot%20images/Screenshot%20(117).png>)
+![Project Preview](</Screenshot%20images/Screenshot%20(118).png>)
+![Project Preview](</Screenshot%20images/Screenshot%20(119).png>)
+
+## Overview
+
+A clone of the popular e-commerce website Amazon. Users can order items, add to cart and see delivery details. This project is for educational and portfolio purposes only and will never be deployed or used for any commercial purposes.
+
+### Design and Inspiration
+
+The design and layout of this project have been inspired by well-known e-commerce websites like Amazon, as I aimed to create a visually appealing and functional user interface. However, it's important to clarify that this project is not an official replica of any existing e-commerce platform.
+
+### Code and Functionality
+
+While I have used this project to demonstrate my ability to develop various features found on e-commerce websites, I want to emphasize that the code, scripts, and functionalities presented here have been developed by me from scratch. Any similarities to existing websites are purely coincidental, and I have taken measures to ensure that this project does not infringe on any copyright or intellectual property rights.
+
+## Project Details
+
+- **Frontend:** HTML, CSS, JavaScript
+- **Backend:**
+- **Database:**
+
+**Disclaimer:** I am not affiliated with any existing e-commerce platform, and this project is not endorsed or approved by any such platform.
--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,30 +1,3 @@
-const products = [{
-    image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-    name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-    rating: {
-        stars: 4.5,
-        count: 87
-    },
-    priceCents: 1090
-},
-{
-    image: "images/products/intermediate-composite-basketball.jpg",
-    name: 'Intermediate Size Basketball',
-    rating: {
-        stars: 4,
-        count: 127
-    },
-    priceCents: 2095
-},
-{
-    image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
-    name: ' Adults Plain Cotton T-Shirt - 2 Pack',
-    rating: {
-        stars: 4.5,
-        count: 56
-    },
-    priceCents: 799
-}];
 let productsHTML = '';
 products.forEach((product) => {
     productsHTML += `<div class="product-container">
@@ -71,10 +44,41 @@ products.forEach((product) => {
         Added
         </div>
 
-        <button class="add-to-cart-button button-primary">
+        <button class="add-to-cart-button button-primary js-add-to-cart" data-product-id = "${product.id}">
         Add to Cart
         </button>
   </div>`;
-})
+});
 
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
+
+document.querySelectorAll('.js-add-to-cart').forEach((button) => {
+    button.addEventListener('click', () => {
+        const productId = button.dataset.productId;
+        // Check if an item already exists in the cart.
+        let matchingItem;
+        cart.forEach((item) => {
+            if (productId === item.productId) {
+                matchingItem = item;
+            }
+        });
+        // If the item exists, increment its quantity by 1
+        // If not, add the item to the cart
+        if (matchingItem) {
+            matchingItem.quantity += 1;
+        }
+        else {
+            cart.push({
+                productId: productId,
+                quantity: 1
+            });
+        }
+        //Calculate the cart quantity
+        let cartQuantity = 0;
+        cart.forEach(item => {
+            cartQuantity += item.quantity;
+        });
+        // Update the cart quantity on the html
+        document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+    })
+});
