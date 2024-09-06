8564463b9a55df4354b838a2bcbf9a54eed832f9
paul-murithi <paulmurithi233@gmail.com>
2024-09-06 19:34:48 +0300

    Initial backend setup

---
+++ backend/src/test/java/com/paul/amazon/AmazonCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paul.amazon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmazonCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
